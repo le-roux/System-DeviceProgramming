e8252c4dfedc9d46479a9987e255a716ad8a440b
sylvain <sylvain@debian>
2016-03-09 17:17:13 +0100

    Suite de l'exercice 3

---
+++ Lab2/Lab2.3_2.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+typedef struct {
+	int cnt;
+	pthread_mutex_t mutex;
+} barrier;
+
+void thread_func(int i);
+
+barrier b;
+float *v, *v1, *v2, *mat;
+int k;
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		printf("Syntax : %s k\n", argv[0]);
+		return 0;
+	}
+
+	k = atoi(argv[1]);
+	//Create all the variables
+	v1 = (float*) malloc(k * sizeof(float));
+	v2 = (float*) malloc(k * sizeof(float));
+	mat = (float*) malloc(k * k * sizeof(float));
+
+	//Generate all the random numbers
+	srand(time(NULL));
+	for (int i = 0; i < k; i++) {
+		v1[i] = ((float)(rand() % 100) / 100) - 0.5;
+	}
+	
+	for (int i = 0; i < k; i++) {
+		v2[i] = ((float)(rand() % 100) / 100) - 0.5;
+	}
+
+	for(int i = 0; i < k; i++) {
+		for (int j = 0; j < k; j++) {
+			mat[k * i + j] = ((float)(rand() % 100) / 100) - 0.5;
+		}
+	}
+
+	pthread_mutex_lock(&b.mutex);
+	b.cnt = k;
+	pthread_mutex_unlock(&b.mutex);
+
+	/*
+	 * Work
+	 */
+
+	//v = mat * v2
+	v = (float*) malloc(k * sizeof(float));
+	pthread_t thread;
+	for (int i = 0; i < k; i++) {
+		pthread_create(&thread, NULL, (void*)thread_func,(void**) i);
+	}
+	pthread_exit(0);
+}
+
+void thread_func(int i) {
+	v[i] = 0;
+	for (int j = 0; j < k; j++) {
+		v[i]+= mat[k * i + j] * v2[i];
+	}
+	pthread_mutex_lock(&b.mutex);
+	b.cnt--;
+	//Last thread to terminate
+	if (b.cnt == 0) {
+	//result = T(v1) * v
+	float result = 0;
+	for (int i = 0; i < k; i++) {
+		result += v1[i] * v[i];
+	}
+	printf("result = %f\n", result);
+	}
+	pthread_mutex_unlock(&b.mutex);
+}
