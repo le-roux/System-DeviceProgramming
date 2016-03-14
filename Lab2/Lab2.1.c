63617309157e3e5461b15555c2ebc5e3c9178d10
sylvain <sylvain@debian>
2016-03-14 21:57:55 +0100

    Amélioration de l'implémentation du mutex

--- Lab2/Lab2.1.c
+++ Lab2/Lab2.1.c
@@ -3,11 +3,14 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-int* vet;
-sem_t* mutex;
+typedef struct Data {
+	int* vet;
+	pthread_mutex_t mutex;
+} Data;
 
-void 
-merge(int *vet, int left, int middle, int right) {
+Data data;
+
+void merge(int *vet, int left, int middle, int right) {
     int i, j, k;
     int n1 = middle - left + 1;
     int n2 =  right - middle;
@@ -36,13 +39,18 @@ merge(int *vet, int left, int middle, int right) {
     while (j < n2)
       vet[k++] = R[j++];
 }
- 
+
+/*
+ * Implement the sort algorithm using multiple threads
+ */
 void threaded_sort(int* arg) {
 	int left, right, middle;
 	left = arg[0];
 	right = arg[1];
+
 	if (left < right) {
 		middle = left + (right - left)/2;
+		//Creation of the two threads
 		pthread_t thread1, thread2;
 		int arg1[2] = {left, middle};
 		pthread_create(&thread1, NULL,(void*) threaded_sort,(void*) arg1);
@@ -52,9 +60,9 @@ void threaded_sort(int* arg) {
 		int* ret;
 		pthread_join(thread1,(void**) ret);
 		pthread_join(thread2,(void**) ret);
-		sem_wait(mutex);
-		merge(vet, left, middle, right);
-		sem_post(mutex);
+		pthread_mutex_lock(&data.mutex);
+		merge(data.vet, left, middle, right);
+		pthread_mutex_unlock(&data.mutex);
 	}
 }
 
@@ -62,29 +70,28 @@ int main(int argc, char ** argv) {
   int i, n, len;
 
   if (argc != 2) {
-    printf ("Syntax: %s dimension", argv[0]);
+    printf ("Syntax: %s dimension\n", argv[0]);
     return (1);
   }
   
   n = atoi(argv[1]);
-
-  vet = (int*) malloc(n * sizeof(int));
   
-  mutex = (sem_t*) malloc(sizeof(sem_t));
-  sem_init(mutex, 0, 1);
-  sem_wait(mutex);
+  data.vet = (int*) malloc(n * sizeof(int));
+ 
+  pthread_mutex_init(&data.mutex, NULL);
+  pthread_mutex_lock(&data.mutex);
   for(i = 0;i < n;i++) {
-    vet[i] = rand() % 100;
-	printf("%d\n",vet[i]);
+    data.vet[i] = rand() % 100;
+	printf("%d\n",data.vet[i]);
   }
-  sem_post(mutex);
+  pthread_mutex_unlock(&data.mutex);
     int arg[2] = {0, n-1};
   threaded_sort(arg);
 
   printf("\n");
-  sem_wait(mutex);
+  pthread_mutex_lock(&data.mutex);
   for(i = 0;i < n;i++) 
-	printf("%d\n",vet[i]);
-  sem_post(mutex);
+	printf("%d\n",data.vet[i]);
+  pthread_mutex_unlock(&data.mutex);
   return 0;
 }
